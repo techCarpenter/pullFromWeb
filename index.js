d55e2a6c37f63d5e3cef15f7e1ac1eb82b5709a3
Brian DeVries <bridevries@lifestorage.com>
2022-01-19 11:37:07 -0500

    remove links to stylesheets

--- index.js
+++ index.js
@@ -29,20 +29,50 @@ let webPages = [
 	}
 ];
 
-for (let i = 0; i < webPages.length; i++) {
-	let response = await fetch(webPages[i].url).then(res => res.text());
+async function main() {
+	for (let i = 0; i < webPages.length; i++) {
+		const dom = await getDomForUrl(webPages[i].url);
+		const win = dom.window;
+		const doc = dom.window.document;
 
-	const dom = new JSDOM(response, {
-		url: webPages[i].url
-	});
+		// Remove all script and style tags
+		Array.from(doc.querySelectorAll("script, style")).forEach(item =>
+			item.remove()
+		);
 
-	let sudoDom = dom.window.document;
+		// Replace relative urls with hardcoded url
+		Array.from(doc.querySelectorAll("a"))
+			.filter(anchor => anchor.href.startsWith(win.origin))
+			.forEach(anchor => {
+				anchor.href = anchor.href;
+			});
 
-	Array.from(sudoDom.querySelectorAll("script, dummy, style")).forEach(item =>
-		item.remove()
-	);
+		// Remove links to style sheets
+		Array.from(doc.querySelectorAll("link"))
+			.filter(link => link.rel === "stylesheet")
+			.forEach(link => {
+				link.remove();
+			});
 
-	fs.writeFile(`./Documents/${webPages[i].fileName}.html`, dom.serialize());
+		fs.writeFile(`./Documents/${webPages[i].fileName}.html`, dom.serialize());
+	}
+
+	console.log("Files saved!");
 }
 
-console.log("Files saved!");
+/**
+ * Get the DOM for a url
+ * @param {string} url The url string to fetch
+ * @returns returns a JSDOM object
+ */
+async function getDomForUrl(url) {
+	let response = await fetch(url).then(res => res.text());
+
+	return new JSDOM(response, {
+		url
+	});
+}
+
+main().then(() => {
+	return;
+});
